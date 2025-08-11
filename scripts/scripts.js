5ead2facd24f30937e0f5ce4f940dbfe4bf1b4c0
RAGNAR303 <goncalves303.gt@gmail.com>
2025-08-11 17:16:40 -0300

    Subindo Projeto

---
+++ scripts/scripts.js
@@ -0,0 +1,160 @@
+$(document).ready(function () {
+  $("#mobile_btn").on("click", function () {
+    $("#mobile-menu").toggleClass("active");
+    $("#mobile_btn").find("i").toggleClass("fa-x");
+  });
+  const sections = $("section");
+  const navItems = $(".nav-item");
+
+  $(window).on("scroll", function () {
+    const header = $("header");
+    let activeSectionIndex = 0;
+    const scrollPosition = $(window).scrollTop() - header.outerHeight();
+
+    console.log(scrollPosition);
+
+    if (scrollPosition <= 0) {
+      header.css("box-shadow", "none");
+    } else {
+      header.css("box-shadow", "5px 2px 5px rgba(0,0,0,0.1)");
+    }
+
+    sections.each(function (i) {
+      const section = $(this);
+      const sectionTop = section.offset().top - 86;
+      const sectionBotton = sectionTop + section.outerHeight();
+
+      if (scrollPosition >= sectionTop && scrollPosition < sectionBotton) {
+        activeSectionIndex = i;
+        return false;
+      }
+    });
+    navItems.removeClass("active");
+    $(navItems[activeSectionIndex]).addClass("active");
+  });
+
+  ScrollReveal().reveal("#cta", {
+    origin: "left",
+    delay: 375,
+    duration: 1000,
+    distance: "20%",
+    reset: true,
+  });
+
+  ScrollReveal().reveal("#banner", {
+    origin: "right",
+    delay: 500,
+    duration: 1000,
+    distance: "20%",
+    reset: true,
+  });
+
+  ScrollReveal().reveal("#dishes", {
+    origin: "right",
+    delay: 500,
+    duration: 1000,
+    distance: "20%",
+    reset: true,
+  });
+
+  ScrollReveal().reveal("#testimonial_chef", {
+    origin: "left",
+    delay: 500,
+    duration: 1000,
+    distance: "20%",
+    reset: true,
+  });
+  ScrollReveal().reveal("#testimonials-content", {
+    origin: "right",
+    delay: 500,
+    duration: 1000,
+    distance: "20%",
+    reset: true,
+  });
+  ScrollReveal().reveal(".feedback", {
+    origin: "right",
+    delay: 700,
+    duration: 1000,
+    distance: "20%",
+    reset: true,
+  });
+  ScrollReveal().reveal("footer", {
+    origin: "bottom",
+    delay: 700,
+    duration: 1000,
+    distance: "10%",
+    reset: true,
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const dishes = [
+    {
+      img: "../styles/assets/dish1.png",
+      title: "prato 1",
+      description: "Que delicia com franco e batata",
+      price: "20,00",
+      review: "500",
+      star: "★★★★☆",
+    },
+    {
+      img: "../styles/assets/dish1.png",
+      title: "prato 1",
+      description: "Que delicia com franco e batata",
+      price: "20,00",
+      review: "500",
+      star: "★★★★☆",
+    },
+    {
+      img: "../styles/assets/dish1.png",
+      title: "prato 1",
+      description: "Que delicia com franco e batata",
+      price: "20,00",
+      review: "500",
+      star: "★★★★☆",
+    },
+    {
+      img: "../styles/assets/dish1.png",
+      title: "prato 1",
+      description: "Que delicia com franco e batata",
+      price: "20,00",
+      review: "500",
+      star: "★★★★☆",
+    },
+  ];
+
+  const containerDishes = document.getElementById("dishes");
+
+  dishes.forEach((dish) => {
+    const dishesItem = document.createElement("div");
+    dishesItem.classList.add("dish");
+
+    dishesItem.innerHTML = `
+      
+                    <div class="dish-herts">
+                        <i class="fa-solid fa-heart"></i>
+                    </div>
+                    <img src=${dish.img} alt=${dish.title} class="dish-image" width="170px">
+                    <h3 class="dish-title">
+                        ${dish.title}
+                    </h3>
+                    <span class="dish-description">
+                       ${dish.description}
+                    </span>
+                    <div class="dish-rate">
+                        <span>${dish.star}</span>
+                        <span>(${dish.review}+)</span>
+                    </div>
+                    <div class="dish-price">
+                        <h4>R$ ${dish.price}</h4>
+                        <button class="btn-default">
+                            <i class="fa-solid fa-basket-shopping"></i>
+                        </button>
+                    </div>
+               
+    
+    `;
+
+    containerDishes.appendChild(dishesItem);
+  });
+});
